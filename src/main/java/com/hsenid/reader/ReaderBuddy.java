21bbcdd5278c8e181aa04f6621b4cfef03110410
0004072 <kanchana@hsenidlanka.com>
2017-01-16 10:10:18 +0530

    Initial Commit: Abstract project structure

---
+++ src/main/java/com/hsenid/reader/ReaderBuddy.java
@@ -0,0 +1,13 @@
+package com.hsenid.reader;
+
+/**
+ * Hello world!
+ *
+ */
+public class ReaderBuddy
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
