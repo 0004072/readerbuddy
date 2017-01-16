0aabded8612e0d2f40ae1a39ca85610247a8923d
0004072 <kanchana@hsenidlanka.com>
2017-01-16 10:03:26 +0530

    Initial Commit: Abstract project structure

---
+++ src/test/java/com/hsenid/reader/ReaderBuddyTest.java
@@ -0,0 +1,38 @@
+package com.hsenid.reader;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ReaderBuddyTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ReaderBuddyTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ReaderBuddyTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
